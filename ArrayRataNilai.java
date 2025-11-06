3bd568e5068039d22318a3d965d8e7e04ad86e50
rochiszigmakinara <rochiszigmakinara@gmail.com>
2025-11-06 11:03:13 +0700

    modifikasi nomor 1

--- ArrayRataNilai.java
+++ ArrayRataNilai.java
@@ -6,10 +6,15 @@ public class ArrayRataNilai {
         int[] nilaiMhs= new int[10];
         double total=0;
         double rata2;
+        int lulus=0;
 
         for(int i=0; i<nilaiMhs.length; i++) {
-            System.out.print("Masukkan nilai mahasiswa ke-"+i+": ");
+            System.out.print("Masukkan nilai mahasiswa ke-"+(i+1)+": ");
             nilaiMhs[i]= sc.nextInt();
+
+            if (nilaiMhs[i]>70) {
+                lulus++;
+            }
         }
 
         for(int i=0; i<nilaiMhs.length; i++) {
@@ -18,5 +23,6 @@ public class ArrayRataNilai {
 
         rata2= total/nilaiMhs.length;
         System.out.println("Rata-rata nilai= "+rata2);
+        System.out.println("Jumlah mahasiswa yang lulus: " +lulus);
     }
 }
